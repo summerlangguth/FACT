35607d1d74ff8f1297c20ac5fa8a1a97d5681b82
George <141621092+GStaines199@users.noreply.github.com>
2025-09-09 14:46:49 +1000

    createSet Inital commit

---
+++ src/main/java/com/example/FACT/model/KeySets.java
@@ -0,0 +1,50 @@
+package com.example.FACT.model;
+
+public class KeySets {
+    private int id;
+    private String application;
+    private String category;
+    private String difficulty;
+    private String description;
+    private String keyBind;
+
+    public KeySets(String application, String category, String difficulty, String description, String keyBind){
+        this.application = application;
+        this.category = category;
+        this.difficulty = difficulty;
+        this.description = description;
+        this.keyBind = keyBind;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getApplication() {
+        return application;
+    }
+    public void setApplication(String application) {this.application = application;}
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {this.category = category;}
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+    public void setDifficulty(String difficulty) {this.difficulty = difficulty;}
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {this.description = description;}
+
+    public String getKeyBind() {
+        return keyBind;
+    }
+    public void setKeyBind(String keyBind) {this.keyBind = keyBind;}
+}
+
