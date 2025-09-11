2e11eefde47aad47a03a74465b5c569fb3bd2e17
joshXIXI <joshuawhthompson@gmail.com>
2025-09-12 06:16:43 +1000

    Meeting minutes 10-sep

---
+++ src/main/java/com/example/FACT/MainApp.java
@@ -0,0 +1,16 @@
+package com.example.FACT;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/com/example/FACT/model/Application.java
@@ -0,0 +1,5 @@
+package com.example.FACT.model;
+
+public class Application {
+    private String name;
+}
