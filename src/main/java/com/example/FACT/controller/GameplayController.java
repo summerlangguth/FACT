df976f070505975f63982e7d5d7d86d41c20bcf4
summerlangguth <summerlangguth@gmail.com>
2025-09-13 15:24:58 +1000

    added user OS detection method

--- src/main/java/com/example/FACT/controller/GameplayController.java
+++ src/main/java/com/example/FACT/controller/GameplayController.java
@@ -1,4 +1,25 @@
 package com.example.FACT.controller;
 
+import javafx.application.Platform;
+import com.sun.javafx.PlatformUtil;
+
+
 public class GameplayController {
+    private String OS = null;
+
+    /**
+     * returns the operating system the application is being run on.
+     */
+    public void findOS(){
+        if(PlatformUtil.isWindows()){
+            OS = "Windows";
+        } else if (PlatformUtil.isMac()) {
+            OS = "Mac";
+        } else if (PlatformUtil.isLinux()) {
+            OS = "Linux";
+        } else if (PlatformUtil.isUnix()) {
+            OS = "Unix";
+        }
+    }
+
 }
