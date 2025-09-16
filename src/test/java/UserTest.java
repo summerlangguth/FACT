f20a557ddc69783bada9a8d8297f1acce35d27d9
summerlangguth <summerlangguth@gmail.com>
2025-09-16 13:11:48 +1000

    implemented base.fxml for homepage.fxml

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/// immplemented by carley
 public class UserTest {
     private static final String FIRST_NAME = "John";
     private static final String FIRST_NAME_TWO = "Jane";
