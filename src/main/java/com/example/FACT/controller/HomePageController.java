4cd31cff2a96f8537c8bdc426daa73149e6263e4
joshXIXI <joshuawhthompson@gmail.com>
2025-09-17 10:52:15 +1000

    Added demo shortcuts.

--- src/main/java/com/example/FACT/controller/HomePageController.java
+++ src/main/java/com/example/FACT/controller/HomePageController.java
@@ -1,6 +1,8 @@
 package com.example.FACT.controller;
 
 import com.example.FACT.HelloApplication;
+import com.example.FACT.model.GameEngine;
+import com.example.FACT.model.Shortcut;
 import com.example.FACT.model.SqliteUserDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,12 +16,16 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.File;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HomePageController {
@@ -34,6 +40,35 @@ public class HomePageController {
 
     public void onHomePracButton(){
         try{
+
+            // Load homebase layout
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/FACT/gameplay.fxml"));
+            Parent root = loader.load();
+
+            // Get controller of homebase
+            GameplayController controller = loader.getController();
+
+            List<Shortcut> demoShortcuts = List.of(
+                    new Shortcut("Copy", new KeyCodeCombination(KeyCode.C, KeyCombination.SHORTCUT_DOWN)),
+                    new Shortcut("Paste", new KeyCodeCombination(KeyCode.V, KeyCombination.SHORTCUT_DOWN))
+            );
+
+            // Load homepage.fxml into the content area
+            GameEngine engine = new GameEngine(demoShortcuts);
+            controller.setEngine(engine);
+
+            // Get current stage from the login button
+            Stage newStage = new Stage();
+            Stage currentStage = (Stage) homepagePracButton.getScene().getWindow();
+
+            // Set the new scene with the base layout
+            Scene scene = new Scene(root);
+            newStage.setScene(scene);
+            newStage.show();
+            currentStage.hide();
+
+
+            /*
             // Load homebase layout
             FXMLLoader baseLoader = new FXMLLoader(HelloApplication.class.getResource("homebase.fxml"));
             Parent root = baseLoader.load();
@@ -52,6 +87,7 @@ public class HomePageController {
             newStage.setScene(scene);
             newStage.show();
             currentStage.hide();
+            */
         }
         catch(Exception e){
             e.printStackTrace();
